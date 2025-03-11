29ce32bad4352e4302279b621f94eeee2fe2337d
Tentswalo <tintswalototsndlovu@gmail.com>
2025-03-11 09:46:56 +0200

    script p1

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -47,3 +47,28 @@ function saveQuotesToLocalStorage() {
     localStorage.setItem("quotes", JSON.stringify(quotes));
 }
 
+function exportQuotes() {
+    const dataStr = JSON.stringify(quotes, null, 2);
+    const blob = new Blob([dataStr], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "quotes.json";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+}
+
+function importFromJsonFile(event) {
+    const fileReader = new FileReader();
+    fileReader.onload = function(event) {
+      const importedQuotes = JSON.parse(event.target.result);
+      quotes.push(...importedQuotes);
+      saveQuotes();
+      alert('Quotes imported successfully!');
+    };
+    fileReader.readAsText(event.target.files[0]);
+  }
+
